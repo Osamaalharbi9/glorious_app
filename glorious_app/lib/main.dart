0a3aadac318fe7102f872f5391fb90c8695434d2
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2023-08-21 04:39:24 +0300

    first commit(i added image and updated main.dart)

--- glorious_app/lib/main.dart
+++ glorious_app/lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(Myapp());
+void main() => runApp(const Myapp());
+
+//statelesswidget
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                Color.fromARGB(255, 245, 245, 245),
+                Color.fromARGB(255, 205, 205, 205)
+              ])),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            children: [Padding(
+              padding: const EdgeInsets.all(20),
+              child: Image.asset('assets/images/Comp 10 2.png'),
+            ),Text('data')
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
