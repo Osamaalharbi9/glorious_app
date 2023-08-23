910b0da02e5e2712fef0d54e82be153bc296db15
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2023-08-23 15:54:21 +0300

    fifth commit(i added drawer)

--- glorious_app/lib/shop_page.dart
+++ glorious_app/lib/shop_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -11,7 +12,57 @@ class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context)=>IconButton(
+            onPressed: () {Scaffold.of(context).openDrawer();},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            )),),
+      ),
+      drawer: Drawer(backgroundColor: Colors.grey[900],child: Divider(color: Colors.white),),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromARGB(255, 245, 245, 245),
+              Color.fromARGB(255, 205, 205, 205)
+            ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            GNav(
+                color: const Color.fromARGB(255, 162, 162, 162),
+                tabActiveBorder: Border.all(color: Colors.white),
+                tabBackgroundColor: Colors.grey.shade100,
+                mainAxisAlignment: MainAxisAlignment.center,
+                tabBorderRadius: 22,
+                //color: Colors.grey,
+
+                tabs: [
+                  GButton(
+                    icon: Icons.home_rounded,
+                    text: 'Shop',
+                    onPressed: () {},
+                  ),
+                  GButton(
+                    //activeBorder: Border.all(color: Colors.black),
+                    icon: Icons.shopping_bag_rounded,
+                    text: 'Cart',
+                    onPressed: () {},
+                  )
+                ]),
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
